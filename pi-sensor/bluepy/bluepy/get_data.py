63262f32552127a3c315f7419932ee75ba165759
Vicky <vickyd885@gmail.com>
2017-02-11 17:39:49 +0000

    test on pi

--- pi-sensor/bluepy/bluepy/get_data.py
+++ pi-sensor/bluepy/bluepy/get_data.py
@@ -1,11 +1,9 @@
 import time
 
 ## Import the bluepy librarylmao 
-from bluepy.bluepy import btle, sensortag
-
+from bluepy.bluepy import sensortag
 
 tag = sensortag.SensorTag('C4:BE:84:72:67:86')
-
 
 time.sleep(1.0)
 tag.IRtemperature.enable()
@@ -13,6 +11,12 @@ for i in range(5):
     tag.waitForNotifications(1.0)
     print tag.IRtemperature.read()
 
-
 tag.disconnect()
 del tag
+
+
+def enableAllSensors(sensor_id):
+    sensor_id.IRtemperature.enable()
+    sensor_id.IRtemperature.enable()
+def getData():
+    
