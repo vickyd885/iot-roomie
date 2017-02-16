943d8e7cc7977217ecbf56ad5827b71e77879963
vickyd885 <vickyd885@gmail.com>
2017-02-16 13:01:46 +0000

    make script more official, and almost working

--- pi-sensor/bluepy/bluepy/get_data.py
+++ pi-sensor/bluepy/bluepy/get_data.py
@@ -1,22 +1,54 @@
 import time
+import requests # remember to pip install this!
 
-## Import the bluepy librarylmao 
+## Import the bluepy library
 from bluepy.bluepy import sensortag
 
-tag = sensortag.SensorTag('C4:BE:84:72:67:86')
+## Hold list of sensors!
+sensors_list = []
 
-time.sleep(1.0)
-tag.IRtemperature.enable()
-for i in range(5):
-    tag.waitForNotifications(1.0)
-    print tag.IRtemperature.read()
+sensors_list.append( sensortag.SensorTag('C4:BE:84:72:67:86'))
 
-tag.disconnect()
-del tag
+def enable_all_sensors(sensor_tag):
+    sensor_tag.IRtemperature.enable()
+    sensor_tag.humidity.enable()
+    sensor_tag.lightmeter.enable()
+
+def disconnect_from_all_sensors(sensor_tag):
+    sensor_tag.disconnect()
+
+def poll_sensor_tag(sensor_tag):
+
+    # to be changed to something more specific later
+    for(i in range(5)):
+        sensor_tag.waitForNotifications(1.0)
+        temp = sensor_tag.IRtemperature.read()
+        humidity = sensor_tag.humidity.read()
+        light = sensor_tag.lightmeter.read()
+        send_data_via_post(temp, humidity,light)
+
+def send_data_via_post(temp, humidity, light):
+    payload = { 'temp':temp, 'humidity': humidity, 'light': light}
+    r = requests.post("..", data=payload)
+
+def run():
+
+    ## enable Sensors
+    for( sensor_tag in sensors_list):
+        enable_all_sensors(sensor_tag)
+
+    ## sleep for a bit to allow for sensors to be initiatlised
+    time.sleep(1.0)
+
+    for(sensor_tag in sensors_list):
+        poll_sensor_tag(sensor_tag)
+
+    for( sensor_tag in sensors_list):
+        disconnect_from_all_sensors(sensor_tag)
 
 
-def enableAllSensors(sensor_id):
-    sensor_id.IRtemperature.enable()
-    sensor_id.IRtemperature.enable()
-def getData():
-    
+
+
+
+def __main__():
+    def run()
