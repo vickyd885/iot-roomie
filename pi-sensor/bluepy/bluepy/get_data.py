d25396ad3b92a4adf5ed5901a723f17b00e882f6
Vicky <vickyd885@gmail.com>
2017-02-17 17:09:11 +0000

    test sensor tag code

--- pi-sensor/bluepy/bluepy/get_data.py
+++ pi-sensor/bluepy/bluepy/get_data.py
@@ -2,7 +2,11 @@ import time
 import requests # remember to pip install this!
 
 ## Import the bluepy library
-from bluepy.bluepy import sensortag
+import sensortag
+
+## Define server address!
+
+server = "http://localhost:3000/insertData/roomdata"
 
 ## Hold list of sensors!
 sensors_list = []
@@ -17,38 +21,49 @@ def enable_all_sensors(sensor_tag):
 def disconnect_from_all_sensors(sensor_tag):
     sensor_tag.disconnect()
 
+
 def poll_sensor_tag(sensor_tag):
 
-    # to be changed to something more specific later
-    for(i in range(5)):
+
+    for i in range(5):
         sensor_tag.waitForNotifications(1.0)
         temp = sensor_tag.IRtemperature.read()
         humidity = sensor_tag.humidity.read()
         light = sensor_tag.lightmeter.read()
-        send_data_via_post(temp, humidity,light)
+        print "got data:  ", temp, " ", humidity," ", light
+        ppl_count = 1
+        noise  = 0
+        name = "vickys" 
+        
+        send_data_via_post(temp, humidity,light, name, ppl_count, noise)
 
-def send_data_via_post(temp, humidity, light):
-    payload = { 'temp':temp, 'humidity': humidity, 'light': light}
-    r = requests.post("..", data=payload)
+def send_data_via_post(temp, humidity, light, name, ppl_count, noise):
+    
+    payload = { 'temp':temp, 'light': light, 'room': name, 'people': ppl_count}
+    r = requests.post(server, data=payload)
 
 def run():
 
+    print "Hello!"
+    print sensors_list
+
     ## enable Sensors
-    for( sensor_tag in sensors_list):
+    for sensor_tag in sensors_list:
         enable_all_sensors(sensor_tag)
 
+    print "Finished enabling all sensors"
+    
     ## sleep for a bit to allow for sensors to be initiatlised
     time.sleep(1.0)
 
-    for(sensor_tag in sensors_list):
+    
+    for sensor_tag in sensors_list:
         poll_sensor_tag(sensor_tag)
 
-    for( sensor_tag in sensors_list):
+    for sensor_tag in sensors_list:
         disconnect_from_all_sensors(sensor_tag)
 
 
 
 
-
-def __main__():
-    def run()
+run()
