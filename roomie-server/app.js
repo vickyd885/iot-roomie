d0b6cbce44d53b5b5ded38ad18c3dbb389083138
moultz <henry@moultz.com>
2017-03-03 15:10:20 +0000

    Added cors https://github.com/expressjs/cors

--- roomie-server/app.js
+++ roomie-server/app.js
@@ -9,13 +9,16 @@ var index = require('./routes/index');
 
 var viewDB = require('./routes/viewDB');
 var insertDB = require('./routes/insertDB');
-var insertFakeData = require('./routes/insertFakeData');
 
+var cors = require('cors')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+app.use(cors())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/viewDB', viewDB);
 app.use('/insertDB', insertDB);
-app.use('/insertFakeData', insertFakeData);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
